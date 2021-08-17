338c2c060d23617854f5cc53e64a4229506317f5
Mikaelly <mikaelly_sandy@hotmail.com>
2021-08-17 00:05:48 -0300

    Finish the BoardGame object

--- main.js
+++ main.js
@@ -1,7 +1,7 @@
 import "./src/styles/settings/colors.css";
 import "./src/styles/generic/reset.css";
 import "./src/styles/elements/base.css";
-import BoardGame from "./src/objects/BoardGame";
+import BoardGame from "./src/objects/BoardGame/BoardGame";
 
 const $root = document.querySelector("#root");
 const $htmlBoardGame = BoardGame(6);
